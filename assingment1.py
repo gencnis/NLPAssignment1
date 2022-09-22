e77ef5a36c32087840985e3504685c0200a1c06e
AlexWills37 <alexanderwills37@gmail.com>
2022-09-22 15:58:40 -0400

    added dataset, started program

--- assingment1.py
+++ assingment1.py
@@ -1,1 +1,45 @@
-print("Example file hehe")
+import os
+
+def preprocess(text):
+    ''' Preprocesses the text '''
+
+    # Casefold and remove punctuation
+    text = text.casefold()
+    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    
+
+
+    # Removing punctuations in string
+    # Using loop + punctuation string
+    # for character in text:
+    #     if character in punc:
+    #         text = text.replace(character, "")
+
+    for character in punc:
+        text = text.replace(character, "")
+
+    
+    print(text)
+    
+
+
+
+def main():
+    print("Example file hehe")
+
+
+    file_name = "scienceShort.txt"
+
+    assert os.path.exists(file_name), "I was not able to find the file at, "+str(file_name)
+
+    # Open the file as f
+    with open(file_name, 'r', encoding = "utf8") as f:
+        lines = f.read()
+
+        preprocess(" Here is a string with slashes \\\\ \' \" {} ")
+
+
+
+
+if __name__ == "__main__":
+    main()
