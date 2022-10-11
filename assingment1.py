a1907f15474c2eb30c0437445489d20519e55b9c
Nisanur Genc <nisanur.genc19@ncf.edu>
2022-10-10 23:06:05 -0400

    updated assignment1.py

--- assingment1.py
+++ assingment1.py
@@ -24,7 +24,7 @@ def preprocess(text):
     punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     
     for character in punc:
-        text = text.replace(character, "")
+        text = text.replace(character, " ")
 
     # Tokenize and lemmatize
     tokens = nltk.word_tokenize(text)
@@ -117,46 +117,52 @@ def sentence_probability(sentence: str, ngram_model: list[dict], total_size):
 
 
     ngram_size = len(ngram_model)
-    sentence_tokens = preprocess(sentence)
     
+    sentence_tokens = preprocess(sentence)
 
-    conditional_prob: float
+    if len(sentence_tokens) != 0:
+        conditional_prob: float
 
-    sliding_window = []
-    sliding_window.append(sentence_tokens[0])
+        sliding_window = []
+        sliding_window.append(sentence_tokens[0])
 
-    # Add the first word, which is not a conditional probability
-    cumulative_probability = ngram_probability_smooth((sentence_tokens[0]), ngram_model[0], total_size)
+        # Add the first word, which is not a conditional probability
+        cumulative_probability = ngram_probability_smooth((sentence_tokens[0]), ngram_model[0], total_size)
 
-    # Calculate the first ngram probabilities until the sliding window is full (based on ngram size)
-    # NOTE: right now, this model does not place tags to start the sentence / end the sentence.
-    # So for the beginning of the sentence "a b c d" with a trigram model, we are looking at (a), (a b), (a b c), (b c d) INSTEAD OF ('' '' a), ('' a b) (a b c) etc.
-    for i in range(1, min(len(sentence_tokens), ngram_size)):
-        # Add the next word and find the probability of the n-gram
-        sliding_window.append(sentence_tokens[i])
+        # Calculate the first ngram probabilities until the sliding window is full (based on ngram size)
+        # NOTE: right now, this model does not place tags to start the sentence / end the sentence.
+        # So for the beginning of the sentence "a b c d" with a trigram model, we are looking at (a), (a b), (a b c), (b c d) INSTEAD OF ('' '' a), ('' a b) (a b c) etc.
+        for i in range(1, min(len(sentence_tokens), ngram_size)):
+            # Add the next word and find the probability of the n-gram
+            sliding_window.append(sentence_tokens[i])
 
 
-        conditional_prob = ngram_probability_smooth(tuple(sliding_window), ngram_model[len(sliding_window) - 1], total_size )  # Uses the ngram model based on the size of the window. Monogram -> bigram -> ... -> n-gram
-        # For the conditional part, we divide by the probability of everything before the new word (this should be greater than or equal to the probability of the window with the new word)
-        conditional_prob /= ngram_probability_smooth(tuple( sliding_window[0:-1]), ngram_model[len(sliding_window) - 2], total_size )
-        
-        cumulative_probability *= conditional_prob
+            conditional_prob = ngram_probability_smooth(tuple(sliding_window), ngram_model[len(sliding_window) - 1], total_size )  # Uses the ngram model based on the size of the window. Monogram -> bigram -> ... -> n-gram
+            # For the conditional part, we divide by the probability of everything before the new word (this should be greater than or equal to the probability of the window with the new word)
+            conditional_prob /= ngram_probability_smooth(tuple( sliding_window[0:-1]), ngram_model[len(sliding_window) - 2], total_size )
+            
+            cumulative_probability *= conditional_prob
 
-    # Now slide the window the rest of the way
-    for i in range(ngram_size, len(sentence_tokens)):
-        # Slide the window one word over
-        sliding_window.pop(0)
-        sliding_window.append(sentence_tokens[i])
+        # Now slide the window the rest of the way
+        for i in range(ngram_size, len(sentence_tokens)):
+            # Slide the window one word over
+            sliding_window.pop(0)
+            sliding_window.append(sentence_tokens[i])
 
-        # Find probability of the new word in the sentence, given the previous words in the window.
-        if(len(ngram_model) == 1):
-            # Monogram model does not use conditional probability
-            cumulative_probability *= ngram_probability_smooth(tuple(sliding_window), ngram_model[0], total_size)
-        else:
-            cumulative_probability *= ngram_probability_smooth(tuple(sliding_window), ngram_model[ngram_size - 1], total_size) \
-                / ngram_probability_smooth(tuple( sliding_window[0:-1]), ngram_model[ngram_size - 2], total_size)
+            # Find probability of the new word in the sentence, given the previous words in the window.
+            if(len(ngram_model) == 1):
+                # Monogram model does not use conditional probability
+                cumulative_probability *= ngram_probability_smooth(tuple(sliding_window), ngram_model[0], total_size)
+            else:
+                cumulative_probability *= ngram_probability_smooth(tuple(sliding_window), ngram_model[ngram_size - 1], total_size) \
+                    / ngram_probability_smooth(tuple( sliding_window[0:-1]), ngram_model[ngram_size - 2], total_size)
 
-    return cumulative_probability
+        return cumulative_probability
+
+    else:
+        # This needs future work : refactoring
+        return None
+
 
 
 
@@ -184,34 +190,30 @@ def main():
         for text_file in text_files:
                 textfile_list.append( os.path.join(folder, text_file))
 
+
+    tokens = []
+
     # goes through every element of the list to open the every file
     for i in textfile_list:
         # opens the file 
         with open(i, 'r', encoding = "utf8") as f:
             lines = f.read()
 
-            tokens = preprocess(lines)
-            total_size = len(tokens)
-
-            unigrams = create_ngram_dict(tokens, 1)
-            bigrams = create_ngram_dict(tokens, 2)
-            trigrams = create_ngram_dict(tokens, 3)
-            fourgrams = create_ngram_dict(tokens, 4)
-
+            tokens.extend(preprocess(lines)) # it adds all the tokens to the list
+          
             # print_dict_stats(unigrams, total_size, 100)
 
-            bigram_model = [unigrams, bigrams]
+    total_size = len(tokens)
 
-            print(str(sentence_probability("Cystic fibrosis is a cringe illness according to the journals.", bigram_model, total_size)))
-            print(str(sentence_probability("Cystic cringe according the to journals a is fibrosis illness.", bigram_model, total_size)))
-
-            print(str(sentence_probability("Cystic fibrosis is a cringe illness according to the journals.", [unigrams], total_size)))
-            print(str(sentence_probability("Cystic cringe according the to journals a is fibrosis illness.", [unigrams], total_size)))
+    unigrams = create_ngram_dict(tokens, 1)
+    bigrams = create_ngram_dict(tokens, 2)
+    # trigrams = create_ngram_dict(tokens, 3)
+    # fourgrams = create_ngram_dict(tokens, 4)
+    bigram_model = [unigrams, bigrams]
 
     # ---------------------------------------------------------------------
     # TRAINING CODE ENDS
     # ---------------------------------------------------------------------
-
 
 
     # ---------------------------------------------------------------------
@@ -236,37 +238,35 @@ def main():
         for text_file in text_files:
                 textfile_list.append( os.path.join(folder, text_file))
 
-    # goes through every element of the list to open the every file
+
+    average_bigram = 0.0
+    average_unigram = 0.0
+    probability_counter = 0
+
+    # goes through every element of the list of files to open the every file
     for i in textfile_list:
         # opens the file 
         with open(i, 'r', encoding = "utf8") as f:
-            lines = f.read()
+            sentences = f.read().split(".")
+            for sentence in sentences:
+                if sentence != "":
+                    bigram_probability = sentence_probability(sentence, bigram_model, total_size)
+                    unigram_probability = sentence_probability(sentence, [unigrams], total_size) 
 
-            tokens = preprocess(lines) 
-            total_size = len(tokens)
-            
+                    if bigram_probability != None:
+                        average_bigram += bigram_probability
+                        average_unigram += unigram_probability
+                        probability_counter += 1
+        
+    
+    average_bigram /= probability_counter
+    average_unigram /= probability_counter
 
-            unigrams = create_ngram_dict(tokens, 1)
-            bigrams = create_ngram_dict(tokens, 2)
-            trigrams = create_ngram_dict(tokens, 3)
-            fourgrams = create_ngram_dict(tokens, 4)
-
-            # print_dict_stats(unigrams, total_size, 100)
-
-            bigram_model = [unigrams, bigrams]
-
-            print(str(sentence_probability("Cystic fibrosis is a cringe illness according to the journals.", bigram_model, total_size)))
-            print(str(sentence_probability("Cystic cringe according the to journals a is fibrosis illness.", bigram_model, total_size)))
-
-            print(str(sentence_probability("Cystic fibrosis is a cringe illness according to the journals.", [unigrams], total_size)))
-            print(str(sentence_probability("Cystic cringe according the to journals a is fibrosis illness.", [unigrams], total_size)))
+    print("AVERAGE LIKELIHOOD BIGRAM:", average_bigram,"\nAVERAGE LIKELIHOOD UNIGRAM:", average_unigram) 
 
     # ---------------------------------------------------------------------
     # TEST CODE ENDS
     # ---------------------------------------------------------------------
-
-
-
 
 
 
